f70f07fff58b3049d31a34a0bf284efc39e87fba
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-11-30 21:01:51 +0100

    Front-end: Changed the Jazz-icon package

--- client/src/components/Header/UserAccount/UserAccount.jsx
+++ client/src/components/Header/UserAccount/UserAccount.jsx
@@ -1,16 +1,35 @@
+import { useCallback } from "react";
 import { useEth } from "../../../contexts/EthContext";
-import { Jazzicon } from "@ukstv/jazzicon-react";
+import Jazzicon, { jsNumberForAddress } from "react-jazzicon";
 import "./UserAccount.css";
 
+// Constants
+const JAZZICON_DIAMETER = 100;
+
+// Component
 function UserAccount() {
 	/* ---- Contexts -------------------------------- */
 	const { state: { account, balance } } = useEth();
-    
+
+	/* ---- Functions ------------------------------- */
+	const onIconLoad = useCallback(el => {
+		if (el) {
+			const svg = el.querySelector("svg");
+
+			if (svg) {
+				svg.setAttribute("viewBox", `0 0 ${JAZZICON_DIAMETER} ${JAZZICON_DIAMETER}`);
+			}
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [account]);
+
 	/* ---- Page content ---------------------------- */
 	return (
 		<div className="user-account">
-			<div className="user-account-pic">
-				{account ? <Jazzicon className="jazzicon" address={account}/> : <span className="user-account-no-pic"/>}
+			<div className="user-account-pic" ref={onIconLoad}>
+				{account ? (
+					<Jazzicon diameter={JAZZICON_DIAMETER} seed={jsNumberForAddress(account)} paperStyles={{ width: "100%", height: "100%" }} svgStyles={{ width: "100%", height: "100%" }}/>
+				) : <div className="user-account-no-pic"/>}
 			</div>
 
 
