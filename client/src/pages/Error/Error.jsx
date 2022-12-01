0ac14ba27a975327b67f0715924b836027520b5b
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-12-01 14:23:17 +0100

    Front-end: Error page

--- client/src/pages/Error/Error.jsx
+++ client/src/pages/Error/Error.jsx
@@ -1,15 +1,40 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
+import { eventOnElement } from "../../helpers/accessibility.js";
+import BonkImage from "../../assets/images/bonk.png";
+import BonkSound from "../../assets/sounds/bonk.mp3";
+import "./Error.css";
 
+// Constants
+const bonkSound = new Audio(BonkSound);
+
+// Page
 function Error() {
 	/* ---- Contexts -------------------------------- */
+	const navigate = useNavigate();
 	const error = useRouteError();
+
+	/* ---- Functions ------------------------------- */
+	const goBack = () => {
+		if (window.history.state && window.history.state.idx > 1) {
+			navigate(-1);
+		} else {
+			navigate("/", { replace: true });
+		}
+	};
+
+	const playBonk = () => { bonkSound.cloneNode(true).play().catch(console.error); };
 
 	/* ---- Page content ---------------------------- */
 	return (
 		<div className="Page Error-page">
-			<h1>Woopsy doopsy!</h1>
-			<p>Our stoopid devs made a mistake.</p>
-			<p>{error.statusText || error.message}</p>
+			<div className="error-box">
+				<button className="back-link" onClick={goBack}>&larr; Retour</button>
+
+				<h1 className="error-title">Woopsy doopsy!</h1>
+				<p>Our stoopid devs made a mistake.</p>
+				<p className="error-code">{error.status} - {error.statusText || error.message}</p>
+				<img className="bonk-dev" src={BonkImage} alt="bonk time!" {...eventOnElement(playBonk)}/>
+			</div>
 		</div>
 	);
 }
