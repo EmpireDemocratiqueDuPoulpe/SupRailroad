813a9ace41a6529b25c3b3ba91e686bd61f284a9
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-12-04 23:11:50 +0100

    Front-end - WIP: Admin page

--- client/src/pages/AdminCorner/AdminCorner.jsx
+++ client/src/pages/AdminCorner/AdminCorner.jsx
@@ -1,7 +1,51 @@
+import { useState, useEffect } from "react";
+import useTickets from "../../hooks/tickets/useTickets.js";
+import Inputs from "../../components/Inputs";
+import "./AdminCorner.css";
+
 function AdminCorner() {
+	/* ---- Contexts -------------------------------- */
+	const tickets = useTickets();
+
+	/* ---- States ---------------------------------- */
+	const [newPrice, setNewPrice] = useState(/** @type {string} */ "");
+
+	/* ---- Functions ------------------------------- */
+	// noinspection JSUnusedLocalSymbols
+	// eslint-disable-next-line no-unused-vars
+	const updatePrice = () => {
+		tickets.setStandardPrice(newPrice).catch(console.error);
+	};
+
+	/* ---- Effects --------------------------------- */
+	useEffect(() => {
+		if (!newPrice && tickets.standardPrice) {
+			setNewPrice(tickets.standardPrice.toString());
+		}
+	}, [newPrice, tickets.standardPrice]);
+
+	/* ---- Page content ---------------------------- */
 	return (
 		<div className="Page AdminCorner-page">
-			Admin page
+			<div className="inner-page">
+				<div className="inner-page-section">
+					<h2 className="inner-page-section-title">Tarification</h2>
+
+					<Inputs.Number
+						label="Prix du ticket (par km) :" postLabel="ETH"
+						step={0.0001}
+						value={newPrice} onChange={p => setNewPrice(p)}
+						disabled={!tickets.standardPrice}
+					/>
+					<Inputs.Checkbox label="Gagner un max d'argent" checked readOnly/>
+				</div>
+
+				<div className="inner-page-section">
+					<h2 className="inner-page-section-title">Cartes de r&eacute;duction</h2>
+
+					&Agrave; faire.
+				</div>
+			</div>
 		</div>
 	);
 }
--- client/src/pages/TravelPlanner/TravelPlanner.jsx
+++ client/src/pages/TravelPlanner/TravelPlanner.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import useTickets from "../../hooks/tickets/useTickets.js";
 import Map from "../../components/Map/Map.jsx";
 
@@ -6,14 +5,7 @@ function TravelPlanner() {
 	/* ---- Contexts -------------------------------- */
 	const tickets = useTickets();
 
-	/* ---- States ---------------------------------- */
-	const [newPrice, setNewPrice] = useState(/** @type {string} */ "");
-
 	/* ---- Functions ------------------------------- */
-	const updatePrice = () => {
-		tickets.setStandardPrice(newPrice).catch(console.error);
-	};
-
 	const calcTicketPrice = async () => {
 		tickets.requestPrice([{ lat: 48840056, long: 2361997 }, { lat: 49878677, long: 2280633 }, { lat: 50285914, long: 2785091 }]).catch(console.error);
 	};
@@ -21,13 +13,6 @@ function TravelPlanner() {
 	const buyTicket = async () => {
 		tickets.buy().catch(console.error);
 	};
-
-	/* ---- Effects --------------------------------- */
-	useEffect(() => {
-		if (!newPrice && tickets.standardPrice) {
-			setNewPrice(tickets.standardPrice.toString());
-		}
-	}, [newPrice, tickets.standardPrice]);
 
 	/* ---- Page content ---------------------------- */
 	return (
@@ -39,11 +24,6 @@ function TravelPlanner() {
 			<button onClick={buyTicket} disabled={!tickets.currentPrice}>
 				Acheter un ticket {tickets.currentPrice && <>({tickets.currentPrice} ETH)</>}
 			</button>
-
-			<p>[================]</p>
-
-			<input type="number" step=".001" value={newPrice} onChange={e => setNewPrice(e.target.value)} disabled={!tickets.standardPrice}/>
-			<button onClick={updatePrice} disabled={!newPrice}>Modifier le prix 🤑</button>
 		</div>
 	);
 }
