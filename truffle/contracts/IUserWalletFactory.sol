5db4bbd736f27690ac071be289b9e03bc963fc22
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-12-04 18:17:59 +0100

    Contracts: Filled up the abstract wallet with the missing declarations

--- truffle/contracts/IUserWalletFactory.sol
+++ truffle/contracts/IUserWalletFactory.sol
@@ -10,10 +10,17 @@ abstract contract IUserWalletFactory {
         uint256[] cards;
     }
 
+    /// Mappings
+    mapping (address => UserWallet) userToWallet;
+
     /// Functions
+    function _getWalletOf(address _owner) internal view virtual returns(UserWallet memory);
+
     function getWallet() public view virtual returns(UserWallet memory);
 
-    function _addTicket(address _to, TicketFactory.Ticket memory _ticket) internal virtual;
+    function _addTicket(address _to, TicketFactory.Ticket memory _ticket) internal virtual returns(uint256);
+
+    function getTicket(address _owner, uint256 _id) public view virtual returns(TicketFactory.Ticket memory);
 
     //function _addCard(address _to, CardFactory.Card memory _card) internal virtual;
     function _addCard(address _to, uint256 _cardId) public virtual;
