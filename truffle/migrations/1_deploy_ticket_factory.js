85180258a6c0e3ec4fd4f4bc6bbe581baa6c4f37
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-12-07 10:33:44 +0100

    Contracts: Fixed the build file (only luck made it works since today)

--- truffle/migrations/1_deploy_ticket_factory.js
+++ truffle/migrations/1_deploy_ticket_factory.js
@@ -2,9 +2,14 @@ const UserWalletFactory = artifacts.require("UserWalletFactory");
 const TicketFactory = artifacts.require("TicketFactory");
 const CardFactory = artifacts.require("CardFactory");
 
-module.exports = function (deployer) {
-  deployer.deploy(UserWalletFactory).then(() => {
-    deployer.deploy(CardFactory, UserWalletFactory.address);
-  });
-  deployer.deploy(TicketFactory);
+module.exports = async function (deployer) {
+  // Deploy the user wallet.
+  await deployer.deploy(UserWalletFactory);
+  const UserWalletInstance = await UserWalletFactory.deployed();
+
+  // Deploy the card factory
+  await deployer.deploy(CardFactory, UserWalletInstance.address);
+
+  // Deploy the ticket factory
+  await deployer.deploy(TicketFactory);
 };
