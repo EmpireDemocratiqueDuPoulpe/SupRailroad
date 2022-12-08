e03311b0fd90a59f267753e8488c9633c731743d
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-12-08 12:36:02 +0100

    Front-end: Updated the app logo

--- client/src/components/Header/AppLogo/AppLogo.jsx
+++ client/src/components/Header/AppLogo/AppLogo.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import Logo from "../../../assets/images/logo.jpg";
+import { ReactComponent as Logo } from "../../../assets/images/logo.svg";
 import "./AppLogo.css";
 
 function AppLogo() {
 	/* ---- Page content ---------------------------- */
 	return (
 		<Link className="app-logo" to="/">
-			<img src={Logo} alt="Logo"/>
+			<Logo/>
 		</Link>
 	);
 }
