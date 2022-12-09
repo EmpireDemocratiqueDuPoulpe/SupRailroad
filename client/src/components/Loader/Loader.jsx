5617a64965b163e4cf83d942095bb379bd2ef5fc
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-12-09 10:07:24 +0100

    Front-end: Better loader

--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,7 +1,15 @@
+import PropTypes from "prop-types";
 import "./Loader.css";
 
-function Loader() {
-	return <div className="loader"/>;
+function Loader({ centered }) {
+	return (
+		<div className={`loader${centered ? " centered" : ""}`}>
+			<div className="loader-track"/>
+			<div className="loader-train"/>
+		</div>
+	);
 }
+Loader.propTypes = { centered: PropTypes.bool };
+Loader.defaultProps = { centered: true };
 
 export default Loader;
