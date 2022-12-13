60ac46648e7d1945baaaf8cf197b9a2b83a9de0c
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-12-13 11:22:48 +0100

    Front-end: Standard card styling

--- client/src/components/Cards/Card/Card.jsx
+++ client/src/components/Cards/Card/Card.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import Web3 from "web3";
 import useCardsMarket from "../../../hooks/market/useCardsMarket.js";
 import { eventOnElement } from "../../../helpers/accessibility.js";
 import "./Card.css";
 
-function Card({ id, name, description, discountPercent, approvedTo, price, imageURI }) {
+function Card({ id, name, description, discountPercent, approvedTo, price, imagePath }) {
 	/* ---- States ---------------------------------- */
 	const cardsMarket = useCardsMarket();
 	const [flipped, setFlipped] = useState(/** @type {boolean} */ false);
@@ -29,13 +30,13 @@ function Card({ id, name, description, discountPercent, approvedTo, price, image
 				<div className="card-front">
 					<div className="card-front-content">
 						<div className="card-info">
-							<p className="card-id card-info-line no-event" {...eventOnElement(killEvent)}>id: {id}</p>
-							<p className="card-name card-info-line no-event" {...eventOnElement(killEvent)}>name: {name}</p>
-							<p className="card-description card-info-line no-event" {...eventOnElement(killEvent)}>desc.: {description}</p>
-							<p className="card-discount card-info-line no-event" {...eventOnElement(killEvent)}>discount: {discountPercent}</p>
-							<p className="card-price card-info-line no-event" {...eventOnElement(killEvent)}>price: {price}</p>
-							<p className="card-imageURI card-info-line no-event" {...eventOnElement(killEvent)}>image: {imageURI}</p>
+							<p className="card-name card-info-line no-event" {...eventOnElement(killEvent)}>{name}</p>
+							<p className="card-description card-info-line no-event" {...eventOnElement(killEvent)}>{description}</p>
+							<p className="card-discount no-event" {...eventOnElement(killEvent)}>{discountPercent}%</p>
+							<p className="card-price card-info-line no-event" {...eventOnElement(killEvent)}>Prix d&apos;achat : {Web3.utils.fromWei(price, "ether")} ETH</p>
 						</div>
+
+						{imagePath && <div className="card-image" style={{ backgroundImage: `url(${imagePath})` }}/>}
 					</div>
 
 					<div className="card-bottom"/>
@@ -43,9 +44,21 @@ function Card({ id, name, description, discountPercent, approvedTo, price, image
 
 				<div className="card-back">
 					<div className="card-back-content">
-						<input type="text" placeholder="Adresse de destination" value={approvedAddress} onChange={handleApprovedAddress} {...eventOnElement(killEvent)}/>
-						<button onClick={confirmApproval}>&gt;</button>
-						<p className="card-approved-to card-info-line no-event" {...eventOnElement(killEvent)}>approvedTo: {approvedTo}</p>
+						{(approvedTo && !approvedTo.startsWith("0x0")) ? (
+							<div className="card-approved-to">
+								<p className="no-event" {...eventOnElement(killEvent)}>Carte approuv&eacute;e pour :</p>
+								<p className="address no-event" {...eventOnElement(killEvent)}>{approvedTo}</p>
+							</div>
+						) : (
+							<>
+								<p className="card-approved-to-title no-event" {...eventOnElement(killEvent)}>Envoyer la carte à ...</p>
+
+								<div className="card-approved-to-form">
+									<input type="text" placeholder="Adresse de destination" value={approvedAddress} onChange={handleApprovedAddress} {...eventOnElement(killEvent)}/>
+									<button onClick={confirmApproval} disabled={!approvedAddress}>&gt;</button>
+								</div>
+							</>
+						)}
 					</div>
 
 					<div className="card-bottom"/>
@@ -61,7 +74,7 @@ Card.propTypes = {
 	discountPercent: PropTypes.string.isRequired,
 	approvedTo: PropTypes.string,
 	price: PropTypes.string.isRequired,
-	imageURI: PropTypes.string,
+	imagePath: PropTypes.string,
 };
 
 export default Card;
